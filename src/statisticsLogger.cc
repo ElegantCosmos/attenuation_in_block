7c670128440f0d834bd0b33e4808676e7f959388
Michinari Sakai <michsakai@ucla.edu>
2017-12-21 15:53:16 -0800

    Minor comment edits.

--- src/statisticsLogger.cc
+++ src/statisticsLogger.cc
@@ -12,24 +12,24 @@ StatisticsLogger::StatisticsLogger()
   analysis->OpenFile("test");
 
   analysis->SetFirstHistoId(0);
-  analysis->CreateH1after attenuation ("primary", "Energy of primaries after attenuation (MeV)", 100, 0, 10*MeV); // 0
-  analysis->CreateH1after attenuation ("gamma", "Energy of gamma after attenuation (MeV)", 35, 0, 3.5*MeV); // 1
-  analysis->CreateH1after attenuation ("electron", "Energy of electrons after attenuation (MeV)", 35, 0, 3.5*MeV); // 2
-  analysis->CreateH1after attenuation ("alpha", "Energy of alphas after attenuation (MeV)", 100, 0, 10*MeV); // 3
+  analysis->CreateH1("primary", "Energy of primaries after attenuation (MeV)", 100, 0, 10*MeV); // histogram 0
+  analysis->CreateH1("gamma", "Energy of gamma after attenuation (MeV)", 35, 0, 3.5*MeV); // histogram 1
+  analysis->CreateH1("electron", "Energy of electrons after attenuation (MeV)", 35, 0, 3.5*MeV); // histogram 2
+  analysis->CreateH1("alpha", "Energy of alphas after attenuation (MeV)", 100, 0, 10*MeV); // histogram 3
 
   analysis->CreateNtuple("statistics", "statistics");
   analysis->SetFirstNtupleColumnId(0);
-  analysis->CreateNtupleIColumn("primaryPDGEncoding"); // 0
-  analysis->CreateNtupleIColumn("attenuatorThickness_cm"); // 1
-  analysis->CreateNtupleIColumn("nPrimaries_initial"); // 2
-  analysis->CreateNtupleIColumn("nPrimaries_final"); // 3
-  analysis->CreateNtupleIColumn("nPrimaries_final_aboveROILowerBound"); // 4
-  analysis->CreateNtupleIColumn("nGammas_final"); // 5
-  analysis->CreateNtupleIColumn("nGammas_final_aboveROILowerBound"); // 6
-  analysis->CreateNtupleIColumn("nBetas_final"); // 7 
-  analysis->CreateNtupleIColumn("nBetas_final_aboveROILowerBound"); // 8
-  analysis->CreateNtupleIColumn("nAlphas_final"); // 9
-  analysis->CreateNtupleIColumn("nAlphas_final_aboveROILowerBound"); // 10
+  analysis->CreateNtupleIColumn("primaryPDGEncoding"); // column 0
+  analysis->CreateNtupleIColumn("attenuatorThickness_cm"); // column 1
+  analysis->CreateNtupleIColumn("nPrimaries_initial"); // column 2
+  analysis->CreateNtupleIColumn("nPrimaries_final"); // column 3
+  analysis->CreateNtupleIColumn("nPrimaries_final_aboveROILowerBound"); // column 4
+  analysis->CreateNtupleIColumn("nGammas_final"); // column 5
+  analysis->CreateNtupleIColumn("nGammas_final_aboveROILowerBound"); // column 6
+  analysis->CreateNtupleIColumn("nBetas_final"); // column 7 
+  analysis->CreateNtupleIColumn("nBetas_final_aboveROILowerBound"); // column 8
+  analysis->CreateNtupleIColumn("nAlphas_final"); // column 9
+  analysis->CreateNtupleIColumn("nAlphas_final_aboveROILowerBound"); // column 10
   analysis->FinishNtuple();
 }
 
